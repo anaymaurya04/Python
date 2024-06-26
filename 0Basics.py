4aa1e9c5570d6b8338e27e0a9f203e19a550e8a2
Anay Maurya <anaymaurya04@gmail.com>
2024-06-26 18:58:40 +0530

    Initial commit

---
+++ 0Basics.py
@@ -0,0 +1,7 @@
+def calc(a,b):
+    mean=(a*b)/(a+b)
+    print(mean)
+print("Give two values to find mean")
+x= int(input())
+y=int(input())
+calc(x,y)
---
+++ 1Pattern.py
@@ -0,0 +1,93 @@
+n=5
+for i in range(n):
+    for j in range(n):
+        print("*",end="")
+    print()
+print("\n")
+
+
+for i in range(0,n):
+    for j in range(0,i+1):
+        print("*",end="")
+    print()
+
+print("\n")
+
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print(j, end = " ")
+    print()
+
+print("\n")
+
+for i in range(0,n):
+    for j in range(i,n):
+        print("*",end="")
+    print()
+
+print()
+
+for i in range(n,0,-1):
+    for j in range(1,i+1):
+        print(j,end="")
+    print()
+
+for i in range(n):
+    for j in range(n-i-1):
+        print(" ",end="")
+    for j in range(2*i+1):
+        print("*",end="")
+    print()
+
+for i in range(n):
+    for j in range(i):
+        print(" ", end="")
+    for j in range(2*(n-i)-1):
+        print("*", end="")
+    print()
+
+for i in range(n):
+    for j in range(i + 1):
+        print("*", end="")
+    print()
+for i in range(n - 1, 0, -1):
+    for j in range(i):
+        print("*", end="")
+    print()
+
+print()
+
+count=1
+for i in range(n):
+    for j in range(i + 1):
+        print(count%2,end="")
+        count+=1
+    print()
+
+for i in range(n):
+    for j in range(i + 1):
+        if (i + j) % 2 == 0:
+            print(1, end="")
+        else:
+            print(0, end="")
+    print()
+
+print()
+
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print(j, end="")
+    for j in range(2*(n-i)):
+        print(" ",end="")
+    for j in range(i,0,-1):
+        print(j,end="")
+    print()
+
+print()
+
+count=1
+for i in range(n):
+    for j in range(i+1):
+        print(count, end=" ")
+        count+=1
+    print()
