0a7b6bc5cc8341d19673dd43a487a00a85dad903
Anay Maurya <anaymaurya04@gmail.com>
2024-07-06 02:41:27 +0530

    Update 005Hashing.py

--- 005Hashing.py
+++ 005Hashing.py
@@ -6,3 +6,15 @@ std_id["310"] += name
 
 #49 Group Anagrams(Medium)
 #242 Valid Anagrams(Easy)
+#GFG-Frequency
+def freq(arr,N,p):
+    freq_map = {}
+    for i in arr:
+        if 1 <= i <= N:
+            if i in freq_map:
+                freq_map[i] += 1
+            else:
+                freq_map[i] = 1
+    for i in range(1, N + 1):
+        arr[i - 1] = freq_map.get(i, 0)
+
